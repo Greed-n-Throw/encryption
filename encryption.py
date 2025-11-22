220dc149e2b4280f47cbbdee6cc358f2b5cb956b
Greed n Throw <mateo.rggia117@gmail.com>
2025-11-23 00:34:29 +0100

    refraction of the code

--- encryption.py
+++ encryption.py
@@ -1,91 +1,58 @@
-def encryption(sentence: str, encryption_key: str):
-    sentence_int = []
-    for character in sentence:
-        letter = 0
-        if ord(character) == 32:  # code ASCII for space
-            letter = 0
-        else:
-            letter = ord(character) - 96
-        sentence_int.append(letter)
+def convert_str_into_digit_list(string: str) -> list[int]:
+    int_list = []
+    for char in string:
+        int_list.append(0 if ord(char) == 32 else ord(char) - 96)  # 32 code ASCII for space, 97 for a
 
-    key_int = []
-    for character in encryption_key:
-        letter = 0
-        if ord(character) == 32:  # code ASCII for space
-            letter = 0
-        else:
-            letter = ord(character) - 96
-        key_int.append(letter)
-
-    encrypt_int = []
-    for i in range(0, len(sentence_int), len(key_int)):
-        feur = sentence_int[i:i + len(key_int)]
-        sum = [x + y for x, y in zip(feur, key_int)]
-        encrypt_int.extend(sum)
-
-    encrypt_str = []
-    for digit in encrypt_int:
-        if digit > 26:
-            digit -= 27
-        if digit == 0:
-            letter = 65  # code ASCII for A
-        else:
-            letter = digit + 96
-        lettre_finale = chr(letter)
-
-        encrypt_str.extend(lettre_finale)
-        phrase_encrypt = ''.join(encrypt_str)
-
-    return (phrase_encrypt)
+    return int_list
 
 
-def unencryption(sentence: str, encryption_key: str):
-    sentence_int = []
-    for character in sentence:
-        letter = 0
-        if ord(character) == 65:  # code ASCII for A
-            letter = 0
-        else:
-            letter = ord(character) - 96
-        sentence_int.append(letter)
+def convert_digit_list_into_str(int_list: list[int]) -> str:
+    str_list = []
+    for digit in int_list:
+        str_list.append(chr(32) if digit == 0 else chr(digit + 96))
 
-    key_int = []
-    for character in encryption_key:
-        letter = 0
-        if ord(character) == 32:  # code ASCII for space
-            letter = 0
-        else:
-            letter = ord(character) - 96
-        key_int.append(letter)
-
-    encrypt_int = []
-    for i in range(0, len(sentence_int), len(key_int)):
-        feur = sentence_int[i:i + len(key_int)]
-        sum = [x - y for x, y in zip(feur, key_int)]
-        encrypt_int.extend(sum)
-
-    unencrypt_str = []
-    for digit in encrypt_int:
-        if digit < 0:
-            digit += 27
-        if digit == 0:
-            letter = 32
-        else:
-            letter = digit + 96
-        lettre_finale = chr(letter)
-
-        unencrypt_str.extend(lettre_finale)
-        phrase_unencrypt = ''.join(unencrypt_str)
-
-    return phrase_unencrypt
+    return "".join(str_list)
 
 
-sentence = ("this is a test with this code only spaces and lowercases letters can be encrypted")
-cryptage_key = ("cryptage key")
+def encryption(sentence: str, key: str):
+    sentence = convert_str_into_digit_list(sentence)
+    key = convert_str_into_digit_list(key)
 
-code = encryption(sentence, cryptage_key)
+    while len(key) < len(sentence):
+        key.extend(key)
 
-decode = unencryption(code, cryptage_key)
+    encrypt = []
+    for s, k in zip(sentence, key):
+        comb = s + k
+        encrypt.append(comb if comb < 27 else comb - 27)
 
-print(code)
-print(decode)
+    encrypt = convert_digit_list_into_str(encrypt)
+
+    return encrypt
+
+
+def un_encryption(sentence: str, key: str):
+    sentence = convert_str_into_digit_list(sentence)
+    key = convert_str_into_digit_list(key)
+
+    while len(key) < len(sentence):
+        key.extend(key)
+
+    un_encrypt = []
+    for s, k in zip(sentence, key):
+        comb = s - k
+        un_encrypt.append(comb + 27 if comb < 0 else comb if comb < 27 else comb - 27)
+
+    un_encrypt = convert_digit_list_into_str(un_encrypt)
+
+    return un_encrypt
+
+
+if __name__ == "__main__":
+    sentence = "this is a test with this code only spaces and lowercases letters can be encrypted"
+    cryptage_key = "cryptage key"
+
+    code = encryption(sentence, cryptage_key)
+    decode = un_encryption(code, cryptage_key)
+
+    print(code, "\n\n", decode)
